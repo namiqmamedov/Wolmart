f6bc5a62316d82515028a2d3f7889dbb8b39e094
namiqmamedov <namigmm@code.edu.az>
2024-03-06 07:38:56 +0400

    Updated home page

---
+++ assets/js/countdown.js
@@ -0,0 +1,43 @@
+var count = 25253800;
+var counter = setInterval(timer, 1000);
+function timer() {
+  count = count - 1;
+  if (count <= 0) {
+    clearInterval(counter);
+    //on end counter, what i do!
+    return;
+  }
+  let varDay  = Math.floor(count / 86400)
+  let varHour = Math.floor(count / 3600) % 24
+  let varMin = Math.floor(count / 60) % 60
+  let varSec = count % 60
+      
+  console.log(count);
+  //DAYS
+  countD = varDay;
+  
+  //HOURS
+  if (varHour >= 10) {
+    countH = varHour;
+  } else {
+    countH = '0' + varHour;
+  }
+
+  //MINUTES
+  if (varMin >= 10) {
+    countM = varMin;
+  } else {
+    countM = '0' + varMin;
+  }
+
+  //SECONDS
+  if (varSec == 60) {
+    countS = '00';
+  }else if(varSec >= 10) {
+    countS = varSec;
+  } else {
+    countS = '0' + varSec;
+  }
+  //write on html
+  document.getElementById("timer").innerHTML = countD + ':' + countH + ':' + countM + ':' + countS;
+}
--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -8,3 +8,45 @@ var Swipes = new Swiper('.swiper-container', {
         el: '.swiper-pagination',
     },
 });
+
+var Swipes = new Swiper('.service-box', {
+    loop: true,
+    slidesPerView: 4,
+    paginationClickable: true,
+    spaceBetween: 20,
+    // breakpoints: {
+    //     1920: {
+    //         slidesPerView: 3,
+    //         spaceBetween: 30
+    //     },
+    //     1028: {
+    //         slidesPerView: 2,
+    //         spaceBetween: 30
+    //     },
+    //     480: {
+    //         slidesPerView: 1,
+    //         spaceBetween: 10
+    //     }
+    // },
+});
+
+var Swipes = new Swiper('.deal-wrapper', {
+    loop: false,
+    slidesPerView: 5,
+    paginationClickable: true,
+    spaceBetween: 20,
+    // breakpoints: {
+    //     1920: {
+    //         slidesPerView: 3,
+    //         spaceBetween: 30
+    //     },
+    //     1028: {
+    //         slidesPerView: 2,
+    //         spaceBetween: 30
+    //     },
+    //     480: {
+    //         slidesPerView: 1,
+    //         spaceBetween: 10
+    //     }
+    // },
+});
